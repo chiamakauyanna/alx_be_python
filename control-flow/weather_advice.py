71d55a394ea15054044215f0f2b2f995ce9e9e6b
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-07-17 14:42:29 +0100

    removed unnecessary comments

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,15 +1,5 @@
 current_weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-# match current_weather:
-#   case 'sunny':
-#     print('Wear a t-shirt and sunglasses.')
-#   case 'rainy':
-#     print("Don't forget your umbrella and a raincoat.")
-#   case 'cold':
-#     print("Make sure to wear a warm coat and a scarf.")
-#   case _:
-#     print("Sorry, I don't have recommendations for this weather.")
-
 if current_weather == 'sunny':
   print('Wear a t-shirt and sunglasses.')
 elif current_weather == 'rainy':
