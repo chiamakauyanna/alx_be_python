632f20f2ee3e3a793e8146448a41e028438cb3cf
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-07-26 13:42:45 +0100

    Shopping List Manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,53 @@
+# Requirements:
+# Core Functionality:
+
+# Your script should start with an empty list named shopping_list.
+# Implement functionality to add items to the list, remove items, and display the current list.
+# User Interface:
+
+# Use a loop to continuously display a menu with options to the user until they choose to exit. The menu should offer options to add an item, remove an item, view the list, and exit.
+# For adding items, prompt the user for the item name and append it to shopping_list.
+# For removing items, ask the user for the item name and remove it from shopping_list. If the item is not found, display a message indicating so.
+# To view the list, print each item in shopping_list to the console.
+# Ensure your script handles invalid menu choices gracefully.
+
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+
+def main():
+    shopping_list = []
+
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add_item = input('Input item name: ')
+            shopping_list.append(add_item)
+            print()
+            print('item added to shoping list')
+        elif choice == '2':
+            remove_item = input('What would you like to remove? ')
+            if remove_item in shopping_list:
+                shopping_list.remove(remove_item)
+                print()
+                print('item removed from shopping list')
+            else:
+                print('Item not found in shopping list')
+        elif choice == '3':
+            print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
