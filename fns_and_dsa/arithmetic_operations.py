a2ce7716d87fe51fd23bffbedd4c23163fe5cc82
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2025-07-26 12:55:42 +0100

    adjusted indentation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,16 +6,16 @@
 # Return the result of the arithmetic operation.
 
 def perform_operation(num1, num2, operation):
-  if operation == 'add':
-    return num1 + num2
-  elif operation == 'subtract':
-    return num1 - num2
-  elif operation == 'multiply':
-    return num1 * num2
-  elif operation == 'divide':
-    if num2 == 0 :
-      return 'can not divide by zero'
-    else: 
-      return num1 / num2
-  else:
-    return 'invalid operation'
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return 'can not divide by zero'
+        else:
+            return num1 / num2
+    else:
+        return 'invalid operation'
