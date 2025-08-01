bbb5430664da0de99d9067d02284ef77e7f5505c
Chiamaka Uyanna <122683833+chiamakauyanna@users.noreply.github.com>
2025-08-01 18:03:26 +0000

    created a simple bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account = account_balance
+
+
+def deposit(self, amount):
+   self.account_balance += amount
+
+
+def withdraw(self, amount):
+    if self.amount <= 0:
+        self.account_balance -= amount
+        return  True 
+    else:
+        return False
+
+def display(self):
+    print(f"Current Balance: {self.account_balance}")
